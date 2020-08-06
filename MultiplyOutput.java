e5a3a6ea01a323b112f960fbdc5c40494699ed5b
Naomi Isbell <naomiisbell@gmail.com>
2020-08-06 14:22:31 -0400

    initial commit

---
+++ MultiplyOutput.java
@@ -0,0 +1,9 @@
+public class MultiplyOutput {
+    public static void main(String[] args) {
+        int firstNum = 25;
+        int secNum = 5;
+        int result = firstNum * secNum;
+
+        System.out.println(result);
+    }
+}
---
+++ UnitConversion.java
@@ -0,0 +1,8 @@
+public class UnitConversion {
+    public static void main(String[] args) {
+        int inches = 1000;
+        double meters = inches * 0.0254;
+
+        System.out.println(meters);
+    }
+}
